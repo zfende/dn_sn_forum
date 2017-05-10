351080e77032f3b2f21aba5b5ecab7d4a6321ba7
Žiga Fende <zfende@gmail.com>
2017-05-10 17:11:30 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import webapp2
+from handlers.base import MainHandler, CookieAlertHandler
+
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', MainHandler, name="main-page"),
+    webapp2.Route('/set-cookie', CookieAlertHandler, name="set-cookie"),
+], debug=True)
