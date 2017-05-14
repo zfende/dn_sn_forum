212fec531c4f24ed24f4e52288e6b6872556e455
Žiga Fende <zfende@gmail.com>
2017-05-14 19:36:08 +0200

    topics on main page

--- handlers/base.py
+++ handlers/base.py
@@ -2,6 +2,8 @@ import os
 import jinja2
 import webapp2
 
+from google.appengine.api import users
+from models.topic import Topic
 
 template_dir = os.path.join(os.path.dirname(__file__), "../templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
@@ -23,9 +25,18 @@ class BaseHandler(webapp2.RequestHandler):
         if not params:
             params = {}
 
+        # cookies
         cookie_law = self.request.cookies.get("cookie_law")
         if cookie_law:
             params["cookies"] = True
+
+        # google login
+        user = users.get_current_user()
+        if user:
+            params["user"] = user
+            params["logout_url"] = users.create_logout_url('/')
+        else:
+            params["login_url"] = users.create_login_url('/')
 
         template = jinja_env.get_template(view_filename)
         return self.response.out.write(template.render(params))
@@ -33,7 +44,9 @@ class BaseHandler(webapp2.RequestHandler):
 
 class MainHandler(BaseHandler):
     def get(self):
-        return self.render_template("main.html")
+        topics = Topic.query(Topic.deleted == False).fetch()
+        params = {"topics": topics}
+        return self.render_template("main.html", params)
 
 class CookieAlertHandler(BaseHandler):
     def post(self):
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import webapp2
 from handlers.base import MainHandler, CookieAlertHandler
+from handlers.topics import TopicAddHandler, TopicDetailsHandler
 
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler, name="main-page"),
     webapp2.Route('/set-cookie', CookieAlertHandler, name="set-cookie"),
+    webapp2.Route('/topic/add', TopicAddHandler, name="topic-add"),
+    webapp2.Route('/topic/<topic_id:\d+>', TopicDetailsHandler, name="topic-details"),
 ], debug=True)
